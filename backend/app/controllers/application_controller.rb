82c1a62c249655cfbe9691172dfd4f9764c0572c
Hara Misaki <m.kevavu05@gmail.com>
2024-08-07 16:40:30 +0900

    Implement Simple SPA

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::API
+  def hello
+    render json: { message: "test successful" }, status: 200
+  end
 end
--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -5,12 +5,14 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    # 許可するオリジンのドメインを記載
+    # 今回はNext.jsサーバのドメイン
+    origins "localhost:3001"
+
+    resource "*",
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
